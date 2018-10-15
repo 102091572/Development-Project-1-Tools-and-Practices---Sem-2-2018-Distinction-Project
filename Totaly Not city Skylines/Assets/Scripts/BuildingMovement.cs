7314a7df1132325a08dde2c4e8ff637a6ccb325b
Tom Wright <Tom.Wright1303@gmail.com>
2018-10-16 01:23:35 +1100

    Iteration1 current prototype

---
+++ Totaly Not city Skylines/Assets/Scripts/BuildingMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMovement : MonoBehaviour {
+
+    Vector3 target;
+    Vector3 TruePos;
+    public float gridSpacing;
+    public bool ScriptActive;
+
+    private void Start()
+    {
+        ScriptActive = true;
+    }
+    //While in placement mode the building follows the mouse snapping to the grid.
+    private void LateUpdate()
+    {
+        if (ScriptActive)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                target = hit.point;
+                target = target + new Vector3(0.5f, 0, 0.5f);
+            }
+
+            TruePos.x = Mathf.FloorToInt(target.x / gridSpacing) * gridSpacing;
+            TruePos.z = Mathf.FloorToInt(target.z / gridSpacing) * gridSpacing;
+            TruePos.y = 0;
+
+            this.transform.position = TruePos;
+        }
+    }
+}
