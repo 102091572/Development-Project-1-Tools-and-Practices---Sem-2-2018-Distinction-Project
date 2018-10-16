b98a6698a546b9228317b8daaef3c56cc79401b5
Tom Wright <Tom.Wright1303@gmail.com>
2018-10-16 21:54:33 +1100

    Added unit test file

---
+++ Assets/Iteration1Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class Iteration1Test {
+
+	[Test]
+	public void Iteration1TestSimplePasses() {
+		// Use the Assert class to test conditions.
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator Iteration1TestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
